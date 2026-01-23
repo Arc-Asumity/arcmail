c2ee7a6aafd6f78f2e8adc5ca985116724d7845f
Arc Asumity <arcasumity@hotmail.com>
2026-01-24 05:16:05 +0800

    add(conf): add configuration file manager

---
+++ src/conf.rs
@@ -0,0 +1,45 @@
+// Copyright (c) 2026 Arc Asumity
+// Licensed under the GPLv3 or later License.
+// See LICENSE file for details.
+//
+// src/conf.rs
+// Manage configure file.
+
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SmtpServer {
+    pub domains: Vec<String>,
+    pub bind: Vec<String>,
+    pub ports: Vec<u16>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub smtp_servers: Vec<SmtpServer>,
+}
+
+impl Config {
+    pub fn load_path(path: &str) -> anyhow::Result<Config> {
+        let file = File::open(path)?;
+        Self::load_reader(file)
+    }
+
+    pub fn load_reader<R: std::io::Read>(mut reader: R) -> anyhow::Result<Config> {
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
+        Ok(serde_json::from_slice(&buf)?)
+    }
+
+    pub fn save_path(&self, path: &str) -> anyhow::Result<()> {
+        let tmp = format!("{}.tmp", path);
+        let mut file = File::create(&tmp)?;
+        file.write_all(serde_json::to_vec_pretty(self)?.as_slice())?;
+        std::fs::rename(tmp, path)?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Copyright (c) 2026 Arc Asumity
+// Licensed under the GPLv3 or later License.
+// See LICENSE file for details.
+//
+// src/main.rs
+// Entry point.
+
+mod conf;
+
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    init("example/arcmail.json");
+}
+
+fn init(path: &str) {
+    let config = conf::Config::load_path(path);
+
+    print!("{:?}", config);
+    println!("!");
 }
